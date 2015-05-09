96fcd3773317c7fa215a31130cb1709bd08cce3d
Johannes Pietsch <azique@web.de>
2015-05-09 15:19:07 +0200

    no use for timestamp at the moment, got rid of parameter-rich constructor

--- Slanguee/src/main/java/cc/languee/fileiteration/Sentence.java
+++ Slanguee/src/main/java/cc/languee/fileiteration/Sentence.java
@@ -9,19 +9,9 @@ public class Sentence {
 
 	private String language;
 	private String id;
-	private String timestamp; 
+//	private String timestamp; 
 	private String source;
 	private List<String> words; 
-
-	//private Sentence() {/* empty constructor */};
-
-	public Sentence(String language, String source, String id, String timestamp, List<String> words) {
-		this.language = language;
-		this.source = source;
-		this.id = id;
-		this.timestamp = timestamp;
-		this.words = words;
-	}
 
 	public List<String> getWords() {
 		return words;
@@ -41,14 +31,6 @@ public class Sentence {
 
 	public void setId(String id) {
 		this.id = id;
-	}
-
-	public String getTimestamp() {
-		return timestamp;
-	}
-
-	public void setTimestamp(String timestamp) {
-		this.timestamp = timestamp;
 	}
 
 	public String getSource() {
