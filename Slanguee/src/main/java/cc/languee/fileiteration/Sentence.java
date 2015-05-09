7441b216c90302dc1b3adb1451639edcf642be13
Grycek <adam.grycner@gmail.com>
2015-05-09 15:02:15 +0200

    Getters and setters for Sentence

--- Slanguee/src/main/java/cc/languee/fileiteration/Sentence.java
+++ Slanguee/src/main/java/cc/languee/fileiteration/Sentence.java
@@ -27,4 +27,36 @@ public class Sentence {
 		return words;
 	}
 
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
 }
